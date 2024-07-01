3b62e9c8b4d177353f1e1a66a0f40d370fc83931
Luiza Ferri <luizamanfrediniferri@hotmail.com>
2024-07-01 18:29:56 -0300

    form e button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
 export default function Home() {
+  function handlePartipantAdd() {
+    console.log("Participante adicionado");
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>
-        Nome do evento
-      </Text>
-
-      <Text style={styles.eventDate}>
-        Sexta, 4 de Novembro de 2022.
-      </Text>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <View style={styles.containerForm}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#888"
+        />
+        <TouchableOpacity style={styles.button} onPress={handlePartipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-  )
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,32 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6B6B6B',
     fontSize: 16
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#fff',
+    borderRadius: 8,
+    height: 48,
+    width: '80%',
+    marginRight: 16,
+    marginTop: 24,
+    paddingHorizontal: 16
+  },
+  button:{
+    backgroundColor: 'green',
+    borderRadius: 8,
+    height: 48,
+    width: 48,
+    marginTop: 24,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText:{
+    color: '#FDFCFE',
+    fontSize: 24
+  },
+  containerForm:{
+    width: '100%',
+    flexDirection: 'row',
   }
 })
